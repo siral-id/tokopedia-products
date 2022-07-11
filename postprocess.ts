d2f254d90f1b7efd8d632ba5f5e152a2ea829588
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-07-11 17:02:30 +0700

    chore: change max retry

--- postprocess.ts
+++ postprocess.ts
@@ -22,7 +22,7 @@ const fetchWithRetry = async <T>(
   url: string,
   requestOptions: RequestInit,
   retryCount = 0,
-  maxRetry = 10,
+  maxRetry = 15,
   lastError?: string,
 ): Promise<T> => {
   console.log(`current retry: ${retryCount} & max retry: ${maxRetry}`);
@@ -41,7 +41,13 @@ const fetchWithRetry = async <T>(
   } catch (error) {
     console.error(error);
     await sleep(retryCount);
-    return await fetchWithRetry(url, requestOptions, retryCount + 1, error);
+    return await fetchWithRetry(
+      url,
+      requestOptions,
+      retryCount + 1,
+      maxRetry,
+      error,
+    );
   }
 };
 
